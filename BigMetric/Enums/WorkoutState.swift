56dd1a09d9c7962f840b1a6ae5259e91d8043911
Grant Perry <121777018+Grant-Perry@users.noreply.github.com>
2025-03-30 11:35:44 -0400

    v2.0

--- BigMetric/Enums/WorkoutState.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-/// Represents the overall workout lifecycle state within UnifiedWorkoutManager.
-enum WorkoutState: String {
-   case notStarted
-   case running
-   case paused
-   case ended
-}
--- BigMetric/Structs/AppConstants.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-struct AppConstants {
-   static let appName = "BigMetric"
-
-   static func getVersion() -> String {
-	  return Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "Unknown"
-   }
-}
